036415219d196799033b73bff3f87528139696e4
orhgool <orhgool@gmail.com>
2019-03-21 13:54:14 -0600

    21-03-2019 13:54hrs staticfiles

--- odet/settings.py
+++ odet/settings.py
@@ -107,10 +107,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = ''
+PROJECT_DIR=os.path.dirname(__file__)
+STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = ( os.path.join(PROJECT_DIR,'static/'),)
 
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, "static"),
 #    'path_to_static_directory/static/',
