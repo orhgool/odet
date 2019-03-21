ac3a114d503dd46f12914d2e4350685c85a82fbe
orhgool <orhgool@gmail.com>
2019-03-21 13:12:19 -0600

    21-03-2019 13:12hrs

--- odet/settings.py
+++ odet/settings.py
@@ -107,7 +107,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#    'path_to_static_directory/static/',
+#]
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
